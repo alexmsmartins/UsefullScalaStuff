d1b9eb3a16921e92ee97f329309c9eb76b38ff1d
Alexandre Martins <alexmsmartins@gmail.com>
2011-06-17 00:26:26 +0100

    The mapcar parameter f:(T*) => T does not accept (T T) => T.

--- src/main/scala/alexmsmartins/list/MapFuncitons.scala
+++ src/main/scala/alexmsmartins/list/MapFuncitons.scala
@@ -1,14 +1,14 @@
 package alexmsmartins
 
-class MapFuncitons[T] {
-  def mapcar(f: (T *) => T, lists: List[List[T]]): List[T] = {
+class MapFunctions[T] {
+  def mapcar(f:(T*) => T, args:List[T]*): List[T] = {
     //TODO: check if the list sizes and number of arguments in the function match
-
+    val lists = args.toList
     lists.size match {
       case 2 => mapcar2(f.asInstanceOf[(T, T) => T], lists)
-      /*case 3 => mapcar3 (f, lists)
+      case 3 => mapcar3 (f, lists)
       case 4 => mapcar4 (f, lists)
-      case 5 => mapcar5 (f, lists)
+      /*case 5 => mapcar5 (f, lists)
       case 6 => mapcar6 (f, lists)
       case 7 => mapcar7 (f, lists)
       case 8 => mapcar8 (f, lists)
@@ -34,21 +34,24 @@ class MapFuncitons[T] {
   private def mapcar2(f: (T, T) => T, lists: List[List[T]]): List[T] = {
     for (val index <- List.range(0, (lists.head).asInstanceOf[List[T]].size).asInstanceOf[List[Int]]  ) {
       f(lists(0).asInstanceOf[List[T]](index),
-        lists(1).asInstanceOf[List[T]](index))
+        lists(2).asInstanceOf[List[T]](index))
     }
   }
 
   private def mapcar3(f: (T *) => T, lists: List[List[T]]): List[T] = {
     for (val index <- List.range(0, lists.head.asInstanceOf[List[T]].size).asInstanceOf[List[Int]]  ) {
       f(lists(0).asInstanceOf[List[T]](index),
-        lists(1).asInstanceOf[List[T]](index))
+        lists(1).asInstanceOf[List[T]](index),
+        lists(2).asInstanceOf[List[T]](index))
     }
   }
 
   private def mapcar4(f: (T *) => T, lists: List[List[T]]): List[T] = {
     for (val index <- List.range(0, lists.head.asInstanceOf[List[T]].size).asInstanceOf[List[Int]]  ) {
       f(lists(0).asInstanceOf[List[T]](index),
-        lists(1).asInstanceOf[List[T]](index))
+        lists(1).asInstanceOf[List[T]](index),
+        lists(2).asInstanceOf[List[T]](index),
+        lists(3).asInstanceOf[List[T]](index))
     }
   }
 }
@@ -58,7 +61,8 @@ object tests {
     val a = List(1, 2, 3, 4)
     val b = List(5, 6, 7, 8)
     val c = List(9, 10, 11, 12)
-    val map = new MapFuncitons
-    map.mapcar((x, y, z) => x + y + z, a, b, c)
+    val map = new MapFunctions[Int]
+    val functTest = (x:Int, y:Int, z:Int) => x + y + z
+    map.mapcar(functTest, a, b, c)
   }
 }
