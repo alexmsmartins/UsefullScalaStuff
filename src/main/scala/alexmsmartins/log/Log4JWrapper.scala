6b84483706d24e27b1335dbed22215ed0d3a125b
Alexandre Martins <alexmsmartins@gmail.com>
2011-06-17 03:31:07 +0100

    A class of what will be a functional looking wrapper around Log4J was commited.

---
+++ src/main/scala/alexmsmartins/log/Log4JWrapper.scala
@@ -0,0 +1,13 @@
+package alexmsmartins.log
+
+/**
+ * This class contains calls to Log4J methods return a value instead of simply logging a message.
+ * This allows for more concise code with a more functional look.
+ * User: alex
+ * Date: 17-06-2011
+ * Time: 3:28
+ * To change this template use File | Settings | File Templates.
+ */
+class Log4JWrapper {
+
+}
