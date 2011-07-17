60d3f3e17f15c6f0075390cffa72496521d30757
Alexandre Martins <alexmsmartins@gmail.com>
2011-07-17 21:31:29 +0100

    Updated scala version to 2.9.0. Increased the maximum number of arguments to 10. Added a require() to do some Design by contract. Added tests to check if mapcar:  - works with a sum function (2nd test case)  - throws a IllegalArgumentException if the lists have    different sizes -> This test still fails

--- src/main/scala/alexmsmartins/list/map.scala
+++ src/main/scala/alexmsmartins/list/map.scala
@@ -1,15 +1,24 @@
 package alexmsmartins
 
+import java.util.logging.Logger
+
 package object map {
 
-  //Design by contract
-  def initialAssertions(): Unit = {
-    ""
-  }
-
-
+  //val logger = Logger.getLogger(classOf[map])
   def mapcar[T](f:Any, args:List[T]*): List[T] = {
-
+    val argsize = args.size
+    //logger.debug("sdafsa")
+    require( 
+      if( argsize > 1){
+        val sizes = args.map(_.size)
+        var bool = true
+        for(index <- List.range(0, sizes.size - 2))
+          bool = bool && (sizes(index) == sizes(index+1))
+        bool
+      } 
+      else true,
+    "All list arguments must have the same number of elements")
+    
     def mapcar1(func: Any, lists: List[List[T]]): List[T] = {
       val f = func.asInstanceOf[(T) => T]
       for (index <- List.range(0, lists.head.size)  ) yield {
@@ -56,10 +65,77 @@ package object map {
       }
     }
 
+    def mapcar6(func: Any, lists: List[List[T]]): List[T] = {
+      val f = func.asInstanceOf[(T,T,T,T,T,T) => T]
+      for (index <- List.range(0, lists.head.size)  ) yield {
+        f(lists(0).asInstanceOf[List[T]](index),
+          lists(1).asInstanceOf[List[T]](index),
+          lists(2).asInstanceOf[List[T]](index),
+          lists(3).asInstanceOf[List[T]](index),
+          lists(4).asInstanceOf[List[T]](index),
+          lists(5).asInstanceOf[List[T]](index))
+      }
+    }
+
+    def mapcar7(func: Any, lists: List[List[T]]): List[T] = {
+      val f = func.asInstanceOf[(T,T,T,T,T,T,T) => T]
+      for (index <- List.range(0, lists.head.size)  ) yield {
+        f(lists(0).asInstanceOf[List[T]](index),
+          lists(1).asInstanceOf[List[T]](index),
+          lists(2).asInstanceOf[List[T]](index),
+          lists(3).asInstanceOf[List[T]](index),
+          lists(4).asInstanceOf[List[T]](index),
+          lists(5).asInstanceOf[List[T]](index),
+          lists(6).asInstanceOf[List[T]](index))
+      }
+    }
+
+    def mapcar8(func: Any, lists: List[List[T]]): List[T] = {
+      val f = func.asInstanceOf[(T,T,T,T,T,T,T,T) => T]
+      for (index <- List.range(0, lists.head.size)  ) yield {
+        f(lists(0).asInstanceOf[List[T]](index),
+          lists(1).asInstanceOf[List[T]](index),
+          lists(2).asInstanceOf[List[T]](index),
+          lists(3).asInstanceOf[List[T]](index),
+          lists(4).asInstanceOf[List[T]](index),
+          lists(5).asInstanceOf[List[T]](index),
+          lists(6).asInstanceOf[List[T]](index),
+          lists(7).asInstanceOf[List[T]](index))
+      }
+    }
+
+    def mapcar9(func: Any, lists: List[List[T]]): List[T] = {
+      val f = func.asInstanceOf[(T,T,T,T,T,T,T,T,T) => T]
+      for (index <- List.range(0, lists.head.size)  ) yield {
+        f(lists(0).asInstanceOf[List[T]](index),
+          lists(1).asInstanceOf[List[T]](index),
+          lists(2).asInstanceOf[List[T]](index),
+          lists(3).asInstanceOf[List[T]](index),
+          lists(4).asInstanceOf[List[T]](index),
+          lists(5).asInstanceOf[List[T]](index),
+          lists(6).asInstanceOf[List[T]](index),
+          lists(7).asInstanceOf[List[T]](index),
+          lists(8).asInstanceOf[List[T]](index))
+      }
+    }
+
+    def mapcar10(func: Any, lists: List[List[T]]): List[T] = {
+      val f = func.asInstanceOf[(T,T,T,T,T,T,T,T,T,T) => T]
+      for (index <- List.range(0, lists.head.size) ) yield {
+        f(lists(0).asInstanceOf[List[T]](index),
+          lists(1).asInstanceOf[List[T]](index),
+          lists(2).asInstanceOf[List[T]](index),
+          lists(3).asInstanceOf[List[T]](index),
+          lists(4).asInstanceOf[List[T]](index),
+          lists(5).asInstanceOf[List[T]](index),
+          lists(6).asInstanceOf[List[T]](index),
+          lists(7).asInstanceOf[List[T]](index),
+          lists(8).asInstanceOf[List[T]](index),
+          lists(9).asInstanceOf[List[T]](index))
+      }
+    }
+
     //TODO: check if the list sizes and number of arguments in the function match
-    val sizes = args.map(_.size)
-    val headSize = sizes(0)
-    val tailSizes = sizes.tail
 
 
     //val f = func.asInstanceOf[(T*) => T] 
@@ -70,12 +146,12 @@ package object map {
       case 3 => mapcar3 (f, lists)
       case 4 => mapcar4 (f, lists)
       case 5 => mapcar5 (f, lists)
-      /*case 6 => mapcar6 (f, lists)
+      case 6 => mapcar6 (f, lists)
       case 7 => mapcar7 (f, lists)
       case 8 => mapcar8 (f, lists)
       case 9 => mapcar9 (f, lists)
       case 10 => mapcar10 (f, lists)
-      case 11 => mapcar11 (f, lists)
+      /*case 11 => mapcar11 (f, lists)
       case 12 => mapcar12 (f, lists)
       case 13 => mapcar13 (f, lists)
       case 14 => mapcar14 (f, lists)
--- src/test/scala/alexmsmartins/list/mapSpec.scala
+++ src/test/scala/alexmsmartins/list/mapSpec.scala
@@ -1,6 +1,7 @@
 package alexmsmartins
 import org.specs._
 import alexmsmartins.map._
+import java.lang.IllegalArgumentException
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,13 +13,18 @@ import alexmsmartins.map._
 object mapcarSpec extends Specification {
   description = "the system basic functionalities are"
   "MapFunction" should {
-    //skip("those examples don't pass yet")
     "be able to add lists of integers " in{
       val a = List(1, 2, 3, 4)
       val b = List(5, 6, 7, 8)
       val c = List(9, 10, 11, 12)
       val functTest = (x:Int, y:Int, z:Int) => x + y + z
       mapcar(functTest, a, b, c) must_== List(15,18,21,24)
+      val funcTest2 = (a:Int, b:Int, c:Int, d:Int) => a+b+c+d
+      mapcar(funcTest2, List(1,2,3,4), List(1,1,1,1), List(2,2,2,2), List(1,1,1,1)) must_== List(5,6,7,8)
+    };
+    "throw a IllegalArgumentExcetion if lists of different sizes are used " in{
+      val funcTest2 = (a:Int, b:Int, c:Int, d:Int) => a+b+c+d
+      mapcar(funcTest2, List(1,2,3,4), List(1,1,1,1), List(2,2,2,2), List(1,1,1)) must throwA(new IllegalArgumentException("message"))
     }
   }
 }
