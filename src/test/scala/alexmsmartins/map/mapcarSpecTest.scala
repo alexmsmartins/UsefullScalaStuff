3d17ed57fff66178cca934f5664b5f34a9c371f0
Alexandre Martins <alexmsmartins@gmail.com>
2011-11-10 15:58:57 +0000

    Added logging to the map tests.

--- src/test/scala/alexmsmartins/map/mapcarSpecTest.scala
+++ src/test/scala/alexmsmartins/map/mapcarSpecTest.scala
@@ -1,11 +1,15 @@
 package alexmsmartins
 
-import log.{LoggerSupplier, LoggerWrapper}
+import log.{LoggerSupplier}
 import org.specs._
 import alexmsmartins.map._
 import alexmsmartins.log.LoggerWrapper._
 import alexmsmartins.log.LoggerSupplier
 import java.lang.{IndexOutOfBoundsException, IllegalArgumentException}
+import runner.JUnit4
+
+
+class mapcarSpecTest extends JUnit4(mapcarSpec)
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,14 +29,17 @@ object mapcarSpec extends Specification with LoggerSupplier {
       val c = List(9, 10, 11, 12)
       val functTest = (x:Int, y:Int, z:Int) => x + y + z
       mapcar(functTest, a, b, c) must_== List(15,18,21,24)
+      supplyLog.info("mapcarSpec test 1.")
       val funcTest2 = (a:Int, b:Int, c:Int, d:Int) => a+b+c+d
       mapcar(funcTest2, List(1,2,3,4), List(1,1,1,1), List(2,2,2,2), List(1,1,1,1)) must_== List(5,6,7,8)
+      supplyLog.info("mapcarSpec test 2.")
     };
     "throw a IndexOutOfBoundsException if lists of different sizes are used " in{
       val funcTest2 = (a:Int, b:Int, c:Int, d:Int) => a+b+c+d
       mapcar(funcTest2, List(1,2,3,4), List(1,1,1,1), List(2,2,2,2), List(1,1,1)) must
         throwA[IndexOutOfBoundsException]
+      supplyLog.info("mapcarSpec test 3.")
     }
   }
-  LoggerWrapper.info("mapcarSpec test logging started!")
+  supplyLog.info("mapcarSpec test logging ended!")
 }
